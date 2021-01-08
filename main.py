626c1d5465780eba4b468bcc0812c9d90514c537
Mao Chuan Li <mao.chuan.li@centrality.ai>
2021-01-08 16:40:37 +1300

    parse pod metrics

--- main.py
+++ main.py
@@ -148,9 +148,10 @@ def extract_pods_metrics():
         eprint(record)
 
 def show_data_frame():
+    pd.set_option('display.max_colwidth', -1)
     df = pd.DataFrame(CURRNET_SECRET_OBJ, columns=['namespace', 'pod_name', 'pod_ip', 'substrate_block_height_best', 'substrate_block_height_finalized','substrate_block_height_sync_target'])
     eprint(df)
-    
+
 def loop_work():
     global CURRNET_SECRET_OBJ
     secret_str = get_current_secret_as_str()
